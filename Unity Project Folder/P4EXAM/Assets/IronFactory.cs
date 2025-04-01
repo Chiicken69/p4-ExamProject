c65abdf0f852b58ad1b7f294f1a92b43a65034c9
Panthy <adamcola06@gmail.com>
2025-04-01 10:39:12 +0200

    pushing for archive

--- Unity Project Folder/P4EXAM/Assets/IronFactory.cs
+++ Unity Project Folder/P4EXAM/Assets/IronFactory.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 [System.Serializable]
 public class IronFactory : FactoryBase
 {
+  
+    
+
     private void Awake()
     {
         base.InitializeFactory();
     }
+
+
+
+
 }
--- Unity Project Folder/P4EXAM/Assets/Produktion/Scripts/Factory/FactoryBase.cs
+++ Unity Project Folder/P4EXAM/Assets/Produktion/Scripts/Factory/FactoryBase.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 
 
@@ -16,27 +17,95 @@ public class FactoryBase : MonoBehaviour
     FactoryState state;
     private ItemBase.ItemType ListOfItems;
     List<GameObject> OutputInventory;
+    List<GameObject> InputInventory;
+
+    [Header("Ingredients")]
+    private bool _readyToCraft;
+    // Ingredient amount should be at same index as ingredient list.
+    [SerializeField] List<ItemBase.ItemType> _ingredientList;
+    [SerializeField] List<int> _ingredientAmountForCraft;
+    [SerializeField] ItemBase.ItemType _itemOutputType;
+
+    [Header("Internal mechanics")]
+    [SerializeField] private float _craftingTime;
 
 
 
 
 
 
-public void InitializeFactory()
+    public void InitializeFactory()
     {
         state = FactoryState.Building;
-       
-
     }
 
-    public void CreateOutputInventory()
+    public void ClearOutputInventory()
     {
         OutputInventory.Clear();   
     }
 
-    public void CreateInputInventory()
+    public void CreateOutput()
     {
+        GameObject ObjectToAdd = new GameObject(_itemOutputType.ToString());
+        ObjectToAdd.AddComponent<ItemBase>();
+        OutputInventory.Add(ObjectToAdd); 
+    }
 
+    public void AddItemToInventory(GameObject ObjectToAdd)
+    {
+        InputInventory.Add(ObjectToAdd);
+    }
+
+    public GameObject TakeItemFromOutputInventory()
+    {
+        int i = OutputInventory.Count;
+        GameObject TemporaryObject = OutputInventory[i];
+        OutputInventory.RemoveAt(i);
+        return TemporaryObject;
+    }
+
+    public void ClearInputInventory()
+    {
+        InputInventory.Clear();
+    }
+
+IEnumerator craft()
+    {
+        ClearInputInventory();
+        yield return new WaitForSeconds(_craftingTime);
+
+    }
+
+    private void CheckForCraftingPossible()
+    {
+        if (_readyToCraft)
+        {
+            craft();
+            _readyToCraft = false;
+        }
+    }
+
+    private bool ItemsFilled()
+    {
+        int x = 0;
+        int p = 0;
+
+        for (int i = 0; i < _ingredientList.Count; i++)
+        {
+            x++;
+
+            for (int k = 0; k < InputInventory.Count; k++)
+            {
+                if (InputInventory[i].name == _ingredientList[k].ToString())
+                {
+                    p++;
+                }
+
+            }
+
+           
+        }
+        return true;
     }
 
     
