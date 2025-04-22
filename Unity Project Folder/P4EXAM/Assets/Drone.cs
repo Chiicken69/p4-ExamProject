c83345e2eed2a159d7b65699487a9ea74c57fcf7
Panthy <adamcola06@gmail.com>
2025-04-22 15:30:08 +0200

    working on drones

--- Unity Project Folder/P4EXAM/Assets/Drone.cs
+++ Unity Project Folder/P4EXAM/Assets/Drone.cs
@@ -1,16 +1,51 @@
+using System.Collections;
 using UnityEngine;
 
 public class Drone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _speed;
+    [SerializeField] private bool _carryingItem;
+
+    //private bool moveObjectSmoothIsRunning;
+    //private bool _timeElapsed
+
+    [SerializeField] private Sprite _sprite;
+
+    private void Start()
     {
-        
+        StartCoroutine(MoveDrone(new Vector2(0, 0), new Vector2(5, 5)));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator  MoveDrone(Vector2 PositionA, Vector2 PositionB)
     {
-        
+        Debug.Log("MoveDrone Routine Started");
+        bool _moveObjectSmoothIsRunning = true;
+        float _timeElapsed = 0f;
+        Vector3 pos = new Vector3();
+        float durationByDistance;
+
+
+        durationByDistance = Vector2.Distance(PositionA, PositionB);
+        durationByDistance = durationByDistance / _speed;
+
+        while (_timeElapsed < durationByDistance)
+        {
+           // pos = Vector2.Lerp(PositionA, PositionB, _timeElapsed / durationByDistance);
+            pos.x = Mathf.SmoothStep(PositionA.x, PositionB.x, _timeElapsed / durationByDistance);
+            pos.y = Mathf.SmoothStep(PositionA.y, PositionB.y, _timeElapsed / durationByDistance);
+            print("CoroutinePOSITION: " + pos);
+            
+            this.transform.position = pos;
+
+            _timeElapsed += Time.deltaTime;
+            yield return null;
+
+
+        }
+
+       // coroutine = null;
+        Debug.Log("MoveObjRoutineSTOPPED");
+
     }
+            
 }
