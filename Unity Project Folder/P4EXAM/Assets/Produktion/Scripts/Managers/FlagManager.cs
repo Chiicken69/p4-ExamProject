3fd472bc2ffa24996a06f6c615971c4dbc6ffc1e
Panthy <adamcola06@gmail.com>
2025-04-22 13:13:02 +0200

    asd

--- Unity Project Folder/P4EXAM/Assets/Produktion/Scripts/Managers/FlagManager.cs
+++ Unity Project Folder/P4EXAM/Assets/Produktion/Scripts/Managers/FlagManager.cs
@@ -1,16 +1,59 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FlagManager : MonoBehaviour
 {
     private Vector3 MouseWorldPos;
 
+    [SerializeField] private int _allowedFlagCount;
+    [SerializeField] private List<Vector2> _flagPoints;
+
+
 
 
     private void Update()
     {
+        SetFlags();
+        
+    }
+
+
+
+    private void CleanUpFlags()
+    {
+        if (_flagPoints.Count > _allowedFlagCount)
+        {
+            _flagPoints.RemoveAt(0);
+        }
+    }
+
+    private void SetFlags()
+    {
         MouseWorldPos = InputHandler.Instance.PassMousePosInWorld();
 
-
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            _flagPoints.Add(MouseWorldPos);
+        }
+        CleanUpFlags();
     }
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
 }
+
+
