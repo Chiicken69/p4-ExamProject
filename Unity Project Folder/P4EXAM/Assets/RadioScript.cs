a303fb5ce5ed26aa7c811dc1145bbcf4d41a8de6
ChristofferJelle <christofferjt@gmail.com>
2025-05-05 14:15:43 +0200

    radio

--- Unity Project Folder/P4EXAM/Assets/RadioScript.cs
+++ Unity Project Folder/P4EXAM/Assets/RadioScript.cs
@@ -1,37 +1,52 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+
 using UnityEngine.UI;
-using static BlueprintBook;
+
 
 public class RadioScript : MonoBehaviour
 {
+    [SerializeField] int timeInSeconds;
 
     private bool _closeUI;
-    private Text unreadMessagesText;
+
+    private bool isInDialogMode =false;
+
+    [SerializeField] private Text unreadMessagesText;
     private int amountOfUnread;
-    private Text PersonWhoMessaged;
-    private Button ButtonForMessages;
+    [SerializeField] private Text personWhoMessaged;
+    [SerializeField] private Text dialogText;
+    [SerializeField] private Button buttonForMessages;
     private bool listenerAdded = false;
 
     [SerializeField] private GameObject radioUI;
     //name,Dialog
+    [SerializeField] Queue<DialogList> dialogData = new Queue<DialogList>();
 
-    [SerializeField] List<DialogList> = new List<DialogList>();
+    //[SerializeField] List<DialogList> DialogData = new List<DialogList>();
 
-            [System.Serializable]
+    [System.Serializable]
    public class DialogList
     {
-
+        public string personMessageing;
+        public string dialog;
+        public bool triggered = false; // tracks if it has been added
     }
-
+    DialogList currentDialog;
     // Update is called once per frame
     void Update()
     {
+        timeInSeconds = Mathf.FloorToInt(Time.time);
+
         GetKeyInfo();
         CloseUI();
         CharPicker();
+        DialogController();
+        if (isInDialogMode)
+        {
+            ShowDialog();
+        }
     }
 
     private void GetKeyInfo()
@@ -43,20 +58,72 @@ public class RadioScript : MonoBehaviour
         if (_closeUI == true && radioUI.activeInHierarchy)
         {
             radioUI.SetActive(false);
+            isInDialogMode = false;
         }
     }
     void CharPicker()
     {
-        if (amountOfUnread <= 1)
+        // If there are dialog messages to process
+
+        if (dialogData.Count > 0)
         {
-            if (listenerAdded == false) {
-                ButtonForMessages.onClick.AddListener(() => DialogController());
+            personWhoMessaged.text = currentDialog.personMessageing;
+
+            unreadMessagesText.text = $"You have {dialogData.Count} unread messages.";
+            if (!listenerAdded)
+            {
+                buttonForMessages.onClick.AddListener(() => ShowDialog());
                 listenerAdded = true;
             }
+        }
+        else if (dialogData.Count == 0)
+        {
+
+            unreadMessagesText.text = "You have 0 Messages";
+            personWhoMessaged.text = "No one";
         }
     }
     void DialogController()
     {
+  
+        if ((timeInSeconds >= 0 && timeInSeconds < 3) && (currentDialog == null || !currentDialog.triggered))
+        {
+            EnqueueDialog("Radio Operator", "Hello, are you still there?", true);
+            Debug.Log("I HAVE QUEUED");
 
+            currentDialog = dialogData.Peek();
+        }
+    }
+
+
+    void EnqueueDialog(string person, string dialog, bool hasTriggered)
+    {
+        // Create a new dialog and enqueue it
+        DialogList newDialog = new DialogList
+        {
+            personMessageing = person,
+            dialog = dialog,
+            triggered = hasTriggered
+        };
+
+        dialogData.Enqueue(newDialog);
+    }
+
+    void ShowDialog()
+    {
+        if (!isInDialogMode)
+        {
+            // Display the dialog (e.g., show it in the UI)
+            dialogText.text = currentDialog.dialog;
+
+            // Once the dialog is shown, dequeue it
+            dialogData.Dequeue();
+            isInDialogMode = true;
+
+        }
+        else
+        {
+
+        }
     }
 }
