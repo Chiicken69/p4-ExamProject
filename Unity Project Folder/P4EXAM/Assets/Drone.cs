07651e6409cd2baf8436b53c1a8f9557a5c72157
ChristofferJelle <christofferjt@gmail.com>
2025-05-07 22:14:46 +0200

    uhh it kinda works i think

--- Unity Project Folder/P4EXAM/Assets/Drone.cs
+++ Unity Project Folder/P4EXAM/Assets/Drone.cs
@@ -1,192 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static ItemBase;
-using static UnityEngine.GraphicsBuffer;
-
-
-
-
-enum DroneState { idle, flying, recharging, Processing }
 
 public class Drone : MonoBehaviour
 {
-    private DroneState droneState;
-    private Queue<IEnumerator> MoveCommands = new Queue<IEnumerator>();
-    private int flagIndex;
+    public List<Vector2> flagPoints = new List<Vector2>();
+    public List<GameObject> flagObjects = new List<GameObject>();
+    public GameObject flagPrefab;
+    public int maxFlagCount = 3;
+    public float moveSpeed = 2f;
 
-   
-    
-    private SpriteRenderer _imageSpriteRenderer;
-    [SerializeField] private float _speed;
-    [SerializeField] private bool _carryingItem;
-    [SerializeField] private GameObject _Item;
-
-    [SerializeField] private Sprite _sprite;
-
-    [Header("Motion Parameters")]
-    [SerializeField] float accel = 10f;   // units/sec²
-    [SerializeField] float maxSpeed = 100f;  // units/sec
-    [SerializeField] float stopThreshold = 0.01f; // how close is “at target”?
-
-
-
-    private List<Vector2> assignedFlags = new List<Vector2>();  // Stores flags for each drone
-
-    private void Start()
+    void Start()
     {
         StartCoroutine(PatrolFlags());
     }
 
-    private IEnumerator PatrolFlags()
+    IEnumerator PatrolFlags()
     {
         while (true)
         {
-            List<Vector2> flags = FlagManager.Instance.GetFlagsForDrone(this);
-            if (flags.Count == 0)
+            if (flagPoints.Count == 0)
             {
                 yield return null;
                 continue;
             }
 
-            // Wrap around if we've gone past the last flag
-            if (flagIndex >= flags.Count)
-                flagIndex = 0;
-
-            Vector2 target = flags[flagIndex];
-            yield return StartCoroutine(MoveDroneTo(target));
-
-            flagIndex++;
-        }
-    }
-
-    private IEnumerator MoveDroneTo(Vector2 target)
-    {
-        float speed = 0f;
-        float stopThreshold = 0.01f;
-
-        while (Vector2.Distance(transform.position, target) > stopThreshold)
-        {
-            float remaining = Vector2.Distance(transform.position, target);
-            float stoppingDist = (speed * speed) / (2f * 10f);  // 10f is accel here
-
-            if (remaining > stoppingDist && speed < 100f)
+            for (int i = 0; i < flagPoints.Count; i++)
             {
-                speed += 10f * Time.deltaTime;
-                speed = Mathf.Min(speed, 100f);
-            }
-            else
-            {
-                speed -= 10f * Time.deltaTime;
-                speed = Mathf.Max(speed, 0f);
-            }
-
-            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-            yield return null;
-        }
-
-        // Snap exactly
-        transform.position = target;
-    }
-
-
-    private IEnumerator ItemTransferLogic()
-    {
-        
-        while (true)
-        {
-            if (takeItem() == true)
-            {
-                yield return new WaitForSecondsRealtime(1.5f);
-            }
-            if (DepositItem() == true)
-            {
-                yield return new WaitForSecondsRealtime(1.5f);
-            }
-            yield return null;
-        }
-        
-    }
-
-    private bool takeItem()
-    {
-        if (!_carryingItem)
-        {
-            GameObject tempGB = FactoryManager.Instance.ReturnFactory(this.transform.position);
-
-            if (tempGB == null)
-            {
-                Debug.Log("yo dawg this shit null");
-                return false;
-            }
-            else
-            {
-               _Item =  tempGB.GetComponent<FactoryBase>().TakeItemFromOutputInventory();
-                if (_Item != null)
+                Vector2 targetPos = flagPoints[i];
+                while (Vector2.Distance(transform.position, targetPos) > 0.1f)
                 {
-                    ChangeCarryingState();
-                    return true;
+                    transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+                    yield return null;
                 }
-                
+
+                yield return new WaitForSeconds(0.5f);
             }
 
-        }
-        return false;
-    }
-
-    private bool DepositItem()
-    {
-        if (_carryingItem)
-        {
-            print("yo wtf");
-            GameObject tempGB = FactoryManager.Instance.ReturnFactory(this.transform.position);
-          
-
-            if (tempGB == null)
-            {
-                Debug.Log("this is null. Deposit");
-                return false;
-            }
-            else if (_Item != null)
-            {
-                //tempGB.GetComponent<FactoryBase>().CheckAgainstRecipe(_Item)
-                tempGB.GetComponent<FactoryBase>().AddItemToInventory(_Item);
-               
-                _Item = null;
-                ChangeCarryingState();
-                Debug.Log("Deposited item: " + _Item);
-                return true;
-            }
-
-        }
-        return false;
-    }
-
-   /* private bool IsItemInRecipe(GameObject GB)
-    {
-        FactoryBase FB = GB.GetComponent<FactoryBase>();
-
-        ItemBase IB = _Item.GetComponent<ItemBase>();
-
-
-
-        if ()
-        {
-            
-        }
-    } */
-
-    private void ChangeCarryingState()
-    {
-        if (_Item != null)
-        {
-            _carryingItem = true;
-        }
-        else
-        {
-            _carryingItem= false;
+            yield return null;
         }
     }
 }
-
-
--- Unity Project Folder/P4EXAM/Assets/Produktion/Scripts/Managers/FlagManager.cs
+++ Unity Project Folder/P4EXAM/Assets/Produktion/Scripts/Managers/FlagManager.cs
@@ -1,205 +1,71 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
-public enum mode { Normal, Flag, Blueprint }
 public class FlagManager : MonoBehaviour
 {
-    [SerializeField] public int _allowedFlagCount;
-    [SerializeField] public List<Vector2> _flagPoints;
-    [SerializeField] public GameObject FlagPrefab;
-    [SerializeField] public List<GameObject> FlagObjects;
-    public  static bool _flagmode = false;
-    public mode _mode;
-
     public static FlagManager Instance;
-    private bool Hasrun = false;
+    public bool flagMode = false;
+
+    public Drone selectedDrone;
 
     private void Awake()
     {
-      
-        
-            if (Instance == null)
+        Instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flagMode = !flagMode;
+            Debug.Log("Flag mode: " + flagMode);
+        }
+
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (flagMode && selectedDrone != null)
             {
-                Instance = this;
+                PlaceFlag(mousePos);
             }
             else
             {
-                DestroyImmediate(gameObject);
+                TrySelectDrone(mousePos);
             }
-           
-        
-        
-
-        _flagPoints = new List<Vector2>();
-    }
-
-    private Vector3 MouseWorldPos;
-
-   
-
-
-
-    private void Update()
-    {
-       // SetFlags();
-      
-
-    }
-
-    private void LateUpdate()
-    {
-        print(_flagmode);
-        SetFlags();
-    }
-
-
-
-    private void CleanUpFlags()
-    {
-        if (_flagPoints.Count > _allowedFlagCount)
-        {
-            //DroneManager.Instance.RemoveMoveCommands();
-            _flagPoints.RemoveAt(0);
         }
     }
 
-    private void SetFlags()
+    void TrySelectDrone(Vector2 mousePos)
     {
-       
-        if (Input.GetMouseButtonDown(0)
-            && _flagmode
-            && !EventSystem.current.IsPointerOverGameObject())
-
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+        if (hit.collider != null)
         {
-            MouseWorldPos = InputHandler.Instance.PassMousePosInWorld();
-            
-            Debug.Log("Placing flag at: " + MouseWorldPos);
-            _flagPoints.Add(MouseWorldPos);
-
-            CleanUpFlags();
-            DisplayFlags();
-            
-        }
-
-
-        /*
-        MouseWorldPos = InputHandler.Instance.PassMousePosInWorld();
-        //Debug.Log("MODE IS: " + _mode.ToString());
-        
-        
-            
-            if (Input.GetMouseButtonDown(0))
+            Drone drone = hit.collider.GetComponent<Drone>();
+            if (drone != null)
             {
-                    
-
-                
-
-                if (_flagmode == true)
-                 {
-                _flagPoints.Add(MouseWorldPos);
-                CleanUpFlags();
-                DroneManager.Instance.UpdateDroneMoves();
-
-                 }
+                selectedDrone = drone;
+                Debug.Log("Selected drone: " + drone.name);
+            }
         }
-        
-        
-       */
-
     }
 
-
-    public void ChangeModeToFlagMode()
+    void PlaceFlag(Vector2 pos)
     {
+        var flags = selectedDrone.flagPoints;
+        var flagObjs = selectedDrone.flagObjects;
 
-        //_flagmode = !_flagmode;
-        _flagmode = !_flagmode;
-        Debug.Log("FlagMode is now: " + _flagmode);
-        if (!_flagmode)
+        flags.Add(pos);
+        if (flags.Count > selectedDrone.maxFlagCount)
         {
-            print("SIGMAAAA");
-           //ChangeButtonlook(Color.gray);
-        }
-        if (_flagmode)
-        {
-            print("NOT VERY SIGMAA");
-           // ChangeButtonlook(Color.red);
+            flags.RemoveAt(0);
+            Destroy(flagObjs[0]);
+            flagObjs.RemoveAt(0);
         }
 
+        GameObject flag = Instantiate(selectedDrone.flagPrefab, pos, Quaternion.identity);
+        flagObjs.Add(flag);
+        Debug.Log("Placed flag at: " + pos);
     }
-
-    public void ChangeButtonlook()
-    {
-        GameObject FlagMangButton = GameObject.FindGameObjectWithTag("FlagMangButton");
-        Color PassiveColor = new Color(255, 255, 255, 1f);
-        Color ToggledColor = new Color(255, 255, 255, 0.75f);
-
-        if (!_flagmode)
-        {
-            print("SIGMAAAA");
-            FlagMangButton.GetComponent<UnityEngine.UI.Image>().color = PassiveColor ;
-            //ChangeButtonlook(Color.gray);
-        }
-        if (_flagmode)
-        {
-            print("NOT VERY SIGMAA");
-            FlagMangButton.GetComponent<UnityEngine.UI.Image>().color = ToggledColor;
-           // ChangeButtonlook(Color.red);
-        }
-    }
-
-    public void DisplayFlags()
-    {
-        // 1) Instantiate any new flags we need
-        foreach (var pt in _flagPoints)
-        {
-            // only add if we don’t already have one at exactly that position
-            bool alreadyExists = FlagObjects.Any(obj =>
-                (Vector2)obj.transform.position == pt
-            );
-            if (!alreadyExists)
-                FlagObjects.Add(Instantiate(FlagPrefab, pt, Quaternion.identity));
-        }
-
-        // 2) Collect indices of flag‑objects to remove
-        var toRemove = new List<int>();
-        for (int i = 0; i < FlagObjects.Count; i++)
-        {
-            Vector2 pos = FlagObjects[i].transform.position;
-            // if this position is no longer in _flagPoints, mark it
-            if (!_flagPoints.Contains(pos))
-                toRemove.Add(i);
-        }
-
-        // 3) Destroy & remove them, iterating indices backwards
-        for (int ri = toRemove.Count - 1; ri >= 0; ri--)
-        {
-            int idx = toRemove[ri];
-            Destroy(FlagObjects[idx]);        // schedule GameObject destruction
-            FlagObjects.RemoveAt(idx);        // remove from our list
-        }
-
-    }
-
-
-
-  
-
-
-
-
-
-
-
-
-
 }
-
-
